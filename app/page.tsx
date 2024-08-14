f3af773a4549b057cbad7b8d433a547a312974ab
dcoromina <dcoromina98@gmail.com>
2024-08-14 20:14:27 +0200

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import ContactSection from "./containers/info-section/ContactSection";
 import ProjectsSection from "./containers/info-section/ProjectsSection";
 import CareerSection from "./containers/info-section/CareerSection";
 import ExpApp from "./components/experiencemenu";
-import LangSel from "./components/languageSelector";
 import NavSection from "./components/NavSection/nav2";
 import Footer from "./components/Footer";
 import { NextUIProvider } from "@nextui-org/react";
